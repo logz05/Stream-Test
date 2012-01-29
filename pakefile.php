52a44116465105796edafa2a5ff5775e0a74ef4d
Ben Constable <ben@benconstable.co.uk>
2012-01-29 17:39:23 +0000

    Add Compose to repo

---
+++ pakefile.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("Compose/pake_helpers.php");
+require_once("Compose/default_pakefile.php");
+
+pake_properties("Compose/pake_properties.ini");
+
+pake_desc("Main deploy task");
+pake_task("deploy", "default_deploy");
+function run_deploy($obj, $args)
+{
+	pake_echo_action("DEPLOYED", "** Application has finished deploying");
+}
