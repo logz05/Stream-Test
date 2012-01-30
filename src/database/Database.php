7877a2a445ca57e068ba244ff845c7c7d7421f07
Ben Constable <ben@benconstable.co.uk>
2012-01-30 20:09:56 +0000

    Syntax error in Database class

--- src/database/Database.php
+++ src/database/Database.php
@@ -7,14 +7,14 @@
  */
 class Database
 {
-	private $dbName = "stream_test";
-	private $user = "stream_test";
-	private $password = "str34mt35t";
+	public static $dbName = "stream_test";
+	public static $user = "stream_test";
+	public static $password = "str34mt35t";
 	
 	public static function connect()
 	{
 		try {
-			return new PDO("mysql:dbname={$this->dbName};host=localhost", $this->user, $this->password);
+			return new PDO("mysql:dbname=" . self::$dbName . ";host=localhost", self::$user, self::$password);
 		}
 		catch(Exception $e) {
 			return null;
