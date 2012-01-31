f821ac6073ebe6fa7f07d48370d11d01d7b6ee96
Ben Constable <ben@benconstable.co.uk>
2012-01-31 20:56:20 +0000

    Error testing

--- src/FacebookStream.php
+++ src/FacebookStream.php
@@ -225,15 +225,15 @@ class FacebookStream extends Stream
 				
 				$likes = $this->countLikes($photo["likes"]);
 				
-				$stmt = $this->db->prepare("INSERT INTO facebook_photo (user_id, object_id, object_date, likes) VALUES (?, ?, ?, ?)");
+				$stmt = $this->db->prepare("INSERT INTO facebook_photo (user_id, object_id, object_date, from, source, link, likes) VALUES (?, ?, ?, ?, ?, ?, ?)");
 				
 				$stmt->bindParam(1, $this->userId, PDO::PARAM_INT);
 				$stmt->bindParam(2, $photo["id"], PDO::PARAM_STR);
 				$stmt->bindParam(3, $photo["created_time"], PDO::PARAM_STR);
-				//$stmt->bindParam(4, $photo["from"]["name"], PDO::PARAM_STR);
-				//$stmt->bindParam(5, $photo["source"], PDO::PARAM_STR);
-				//$stmt->bindParam(6, $photo["link"], PDO::PARAM_STR);
-				$stmt->bindParam(4, $likes, PDO::PARAM_INT);
+				$stmt->bindParam(4, $photo["from"]["name"], PDO::PARAM_STR);
+				$stmt->bindParam(5, $photo["source"], PDO::PARAM_STR);
+				$stmt->bindParam(6, $photo["link"], PDO::PARAM_STR);
+				$stmt->bindParam(7, $likes, PDO::PARAM_INT);
 								
 				$stmt->execute();
 			}
