f96025e97a69a401cbabff0d6a2825ba8a2dd4c3
Ben Constable <ben@benconstable.co.uk>
2012-02-09 20:51:47 +0000

    added error reporting

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/src/streams/StreamGetter.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/src/streams/FacebookStream.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/src/streams/TwitterStream.php';
